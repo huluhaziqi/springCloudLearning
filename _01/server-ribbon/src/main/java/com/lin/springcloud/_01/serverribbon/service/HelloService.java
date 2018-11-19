760a7821297e97f8d8dd95db5a6efc8b87e0c4f6
huluhaziqi <huluhaziqi@gmail.com>
2018-11-19 16:28:53 +0800

    add ribbon

---
+++ _01/server-ribbon/src/main/java/com/lin/springcloud/_01/serverribbon/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.lin.springcloud._01.serverribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://client/hi?name=" + name, String.class);
+    }
+}
