1dd6f683074e5cad3e86d4c9faa3f4de2d0c5a99
huluhaziqi <huluhaziqi@gmail.com>
2018-11-19 14:11:29 +0800

    first commit eurka server client
    modify .gitignore

---
+++ _01/client/src/main/java/com/lin/springcloud/_01/client/ClientApplication.java
@@ -0,0 +1,27 @@
+package com.lin.springcloud._01.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "lin") String name) {
+        return "hi " + name + " , I am from port :" + port;
+    }
+}
---
+++ _01/server/src/main/java/com/lin/springcloud/_01/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.lin.springcloud._01.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
